9c00130037a0588bc4fc50e6a197bb768184d872
haripo <haripo@users.noreply.github.com>
2017-05-22 20:47:19 +0900

    Generate ojama images from xcf

---
+++ raw_assets/generate.sh
@@ -0,0 +1,9 @@
+convert noticingPuyos.xcf -crop 16x16 cropped_ojama_%03d.png
+mv cropped_ojama_000.png ../assets/ojama_small.png
+mv cropped_ojama_001.png ../assets/ojama_large.png
+mv cropped_ojama_002.png ../assets/ojama_stone.png
+mv cropped_ojama_003.png ../assets/ojama_mushroom.png
+mv cropped_ojama_004.png ../assets/ojama_star.png
+mv cropped_ojama_005.png ../assets/ojama_crown.png
+rm -f cropped_ojama_*
+
