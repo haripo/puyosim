4f1f9201f4f783031a7b3ae24d5e174d32524964
haripo <haripo@users.noreply.github.com>
2017-05-24 00:20:56 +0900

    Fix image size

--- raw_assets/generate.sh
+++ raw_assets/generate.sh
@@ -1,4 +1,4 @@
-convert noticingPuyos.xcf -crop 16x16 cropped_ojama_%03d.png
+convert noticingPuyos.xcf -crop 16x16 -sample 200% cropped_ojama_%03d.png
 mv cropped_ojama_000.png ../assets/ojama_small.png
 mv cropped_ojama_001.png ../assets/ojama_large.png
 mv cropped_ojama_002.png ../assets/ojama_stone.png
