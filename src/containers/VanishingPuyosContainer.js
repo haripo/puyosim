ab85204151a7ed71e46a7974147b65bf9d74785a
haripo <haripo@users.noreply.github.com>
2017-06-10 13:52:37 +0900

    Fix vanising connection

--- src/containers/VanishingPuyosContainer.js
+++ src/containers/VanishingPuyosContainer.js
@@ -2,11 +2,12 @@ import { connect } from 'react-redux';
 import { applyGravity, finishVanishingAnimations } from '../actions/actions';
 import VanishingPuyos from '../components/VanishingPuyos';
 import toJS from '../utils/toJS';
+import { getVanishingPuyos } from '../reducers/simulator';
 
 const mapStateToProps = (state) => {
   const simulator = state.simulator;
   return {
-    vanishings: simulator.get('vanishingPuyos')
+    vanishings: getVanishingPuyos(simulator)
   };
 };
 
