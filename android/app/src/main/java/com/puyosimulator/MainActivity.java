ff56365e03a87ec70dc9e5013b04d2dce3e917c5
haripo <haripo@users.noreply.github.com>
2017-06-15 01:05:39 +0900

    Import bugsnag

--- android/app/src/main/java/com/puyosimulator/MainActivity.java
+++ android/app/src/main/java/com/puyosimulator/MainActivity.java
@@ -2,6 +2,13 @@ package com.puyosimulator;
 
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
+import com.bugsnag.BugsnagReactNative;
+import android.os.Bundle;
 
 public class MainActivity extends SplashActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    BugsnagReactNative.start(this);
+  }
 }
