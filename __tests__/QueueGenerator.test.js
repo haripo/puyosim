b4ee9e7fc54fd30f72ae97561032b1fe5263d736
haripo <haripo@users.noreply.github.com>
2017-12-16 02:33:54 +0900

    Refactor queue generator

---
+++ __tests__/QueueGenerator.test.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import { limitInitialColors } from '../src/models/QueueGenerator';
+
+describe('limitInitialColors', () => {
+  test('do nothing', () => {
+    let queue = _.fill(Array(32), 1);
+    queue[1] = 1;
+    queue[2] = 2;
+    queue[3] = 3;
+    limitInitialColors(queue, 3, 3);
+
+    expect(queue.slice(0, 4)).toEqual([1, 1, 2, 3])
+  });
+
+  test('fix queue', () => {
+    queue = _.fill(Array(32), 1);
+    queue[0] = 4;
+    queue[1] = 1;
+    queue[2] = 2;
+    queue[3] = 3;
+    limitInitialColors(queue, 3, 3);
+
+    expect(queue.slice(0, 4)).toEqual([4, 1, 2, 1])
+  });
+
+  test('fix queue', () => {
+    queue = _.fill(Array(32), 1);
+    queue[2] = 1;
+    queue[3] = 2;
+    queue[4] = 3;
+    queue[5] = 4;
+    limitInitialColors(queue, 3, 3);
+
+    expect(queue.slice(0, 6)).toEqual([1, 1, 1, 2, 3, 1])
+  });
+
+  test('fix queue with fallback', () => {
+    queue = _.fill(Array(32), 4);
+    queue[0] = 1;
+    queue[1] = 1;
+    queue[2] = 1;
+    queue[3] = 2;
+    queue[4] = 3;
+    queue[5] = 4;
+    limitInitialColors(queue, 3, 3);
+
+    expect(queue.slice(0, 6)).toEqual([1, 1, 1, 2, 3, 1])
+  });
+});
+
--- src/models/QueueGenerator.js
+++ src/models/QueueGenerator.js
@@ -7,11 +7,22 @@ function swapColors(queue: Array<number>, a: number, b: number) {
   return queue;
 }
 
-function rearrangeQueue(queue, tabooColors: Set<number>, limitLength) {
+/**
+ * 与えられた条件で配色制限を満たすように Queue を編集します
+ * @param queue
+ * @param tabooColors 制限色
+ * @param limitLength 制限範囲のぷよ数
+ * @returns {Array<number>} modified queue
+ */
+function rearrangeQueue(queue: Array<number>, tabooColors: Set<number>, limitLength) {
   for (let i = 0; i < limitLength; i++) {
     if (tabooColors.has(queue[i])) {
       const target = _.findLastIndex(queue, color => !tabooColors.has(color));
-      swapColors(queue, i, target);
+      if (limitLength < target) {
+        swapColors(queue, i, target);
+      } else {
+        queue[i] = 1;
+      }
     }
   }
   return queue;
@@ -19,10 +30,21 @@ function rearrangeQueue(queue, tabooColors: Set<number>, limitLength) {
 
 
 /// 初手配色制限
-function limitInitialColors(queue: Array<number>, numColorLimit: number, limitLength: number) {
-  const colors = new Set(queue);
-  const tabooColors = colors.subtract(queue.slice(0, limitLength));
-  return rearrangeQueue(queue, tabooColors, limitLength);
+export function limitInitialColors(
+  queue: Array<number>,
+  numColorLimit: number,
+  numLimitPairs: number) {
+
+  let appeared = new Set();
+  for (let i = 0; i < numLimitPairs * 2; i++) {
+    appeared.add(queue[i]);
+    if (numColorLimit <= appeared.size) {
+      break;
+    }
+  }
+
+  const taboos = new Set(queue.filter(color => !appeared.has(color)));
+  return rearrangeQueue(queue, taboos, numLimitPairs * 2);
 }
 
 /**
@@ -57,7 +79,6 @@ export function generateQueue(configs) {
       queue[0] === queue[2] &&
       queue[0] === queue[3]) {
       return this.generateQueue(configs);
-      // TODO: avoid4ColorsIn3Hands の処理を抽象化すればこれもできそう
     }
   }
 
