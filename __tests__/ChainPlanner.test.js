26eac94bd5fbf1861ca0820c5582d78478f2b3af
haripo <haripo@users.noreply.github.com>
2017-12-21 00:39:14 +0900

    Add chainplanner

---
+++ __tests__/ChainPlanner.test.js
@@ -0,0 +1,65 @@
+import _ from 'lodash';
+import {
+  createChainPlan,
+  getDropPlan,
+  getVanishPlan
+} from '../src/models/ChainPlanner';
+
+describe('getDropPlan', () => {
+  test('generate drop plans', () => {
+    const stack = [
+      [0, 1, 0],
+      [1, 2, 0],
+      [3, 0, 4]
+    ];
+
+    const actual = getDropPlan(stack, 3, 3);
+    const expected = [
+      { row: 1, col: 1, color: 1, distance: 1 },
+      { row: 2, col: 1, color: 2, distance: 1 }
+    ];
+    expect(actual.sort((a, b) => a.row - b.row)).toEqual(expected);
+  });
+});
+
+describe('getVanishPlan', () => {
+  test('generate vanish plans', () => {
+    const stack = [
+      [0, 1, 0, 1, 1],
+      [1, 2, 0, 1, 1],
+      [3, 3, 0, 1, 2],
+      [3, 0, 3, 1, 2],
+      [3, 3, 3, 0, 2],
+    ];
+
+    const actual = getVanishPlan(stack, 5, 5);
+    expect(actual.length).toEqual(2);
+    expect(_.find(actual, connection => connection.color === 3).puyos.length).toEqual(7);
+    expect(_.find(actual, connection => connection.color === 1).puyos.length).toEqual(4);
+  });
+});
+
+describe('createChainPlan', () => {
+  test('generate chain plan', () => {
+    // 19 chain
+    const stack = [
+      [1, 2, 0, 0, 2, 2],
+      [1, 1, 1, 3, 2, 1],
+      [3, 1, 2, 2, 3, 2],
+      [1, 2, 3, 3, 2, 1],
+      [2, 3, 2, 1, 3, 1],
+      [3, 2, 1, 3, 2, 1],
+      [2, 3, 2, 1, 3, 2],
+      [2, 3, 2, 1, 3, 2],
+      [2, 1, 1, 3, 2, 3],
+      [1, 2, 3, 1, 3, 3],
+      [3, 1, 2, 3, 1, 2],
+      [3, 1, 2, 3, 1, 2],
+      [3, 1, 2, 3, 1, 2],
+    ];
+
+    const actual = createChainPlan(stack, 13, 6);
+    expect(actual.chain).toEqual(19);
+    expect(actual.score).toEqual(175080);
+  });
+});
---
+++ src/models/ChainPlanner.js
@@ -0,0 +1,140 @@
+import { calcChainStepScore } from '../utils/scoreCalculator';
+
+type Stack = Array<Array<number>>;
+
+type DroppingPlan = {
+  row: number,
+  col: number,
+  color: number,
+  distance: number
+}
+
+type VanishingPlan = {
+  puyos: Array<{ row: number, col: number }>,
+  color: number
+}
+
+/**
+ * Generate dropping plans, modifying stack
+ * @param stack stack object
+ * @param rows stack size
+ * @param cols stack size
+ * @returns {Array} plans
+ */
+export function getDropPlan(stack: Stack, rows: number, cols: number): Array<DroppingPlan> {
+  let plan = [];
+  for (let i = 0; i < cols; i++) {
+    for (let j = rows - 1; 0 < j; j--) {
+      if (stack[j][i] !== 0) {
+        continue;
+      }
+      let to = j;
+      let from = j;
+      while (0 <= from && stack[from][i] === 0) {
+        from--;
+      }
+      if (0 <= from) {
+        stack[to][i] = stack[from][i];
+        stack[from][i] = 0;
+        plan.push({
+          row: to,
+          col: i,
+          color: stack[to][i],
+          distance: to - from
+        });
+      }
+    }
+  }
+  return plan;
+}
+
+/**
+ * Generate vanishing plans, modifying stack
+ * @param stack
+ * @param rows
+ * @param cols
+ * @returns {Array}
+ */
+export function getVanishPlan(stack: Stack, rows: number, cols: number): Array<VanishingPlan> {
+  const connections = getConnections(stack, rows, cols).filter(c => c.puyos.length >= 4);
+
+  if (connections.length === 0) {
+    return [];
+  }
+
+  let plan = [];
+
+  connections.forEach(connection => {
+    plan.push(connection);
+    connection.puyos.forEach(puyo => {
+      stack[puyo.row][puyo.col] = 0;
+    });
+  });
+
+  return plan;
+}
+
+function getConnections(stack: Stack, rows: number, cols: number) {
+  let connectionIds = Array(rows).fill(null).map(() => Array(cols).fill(0));
+
+  const search = (r, c, puyo) => {
+    // puyos on row = 0 do not be connected nor vanished.
+    if (1 <= r && r < rows && 0 <= c && c < cols &&
+        puyo === stack[r][c] &&
+        !connectionIds[r][c]) {
+      connectionIds[r][c] = id;
+      search(r - 1, c, puyo);
+      search(r, c - 1, puyo);
+      search(r + 1, c, puyo);
+      search(r, c + 1, puyo);
+    }
+  };
+
+  let result = [];
+  let id = 0;
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (stack[row][col] === 0) continue;
+      if (connectionIds[row][col] === 0) {
+        id++;
+        search(row, col, stack[row][col], id);
+
+        connectionIds[row][col] = id;
+        result[id - 1] = {
+          color: stack[row][col],
+          puyos: [{ row, col }]
+        };
+      } else {
+        result[connectionIds[row][col] - 1].puyos.push({ row, col });
+      }
+    }
+  }
+
+  return result;
+}
+
+export function createChainPlan(stack: Stack, rows: number, cols: number) {
+  let result = {
+    plan: [],
+    score: 0,
+    chain: 0
+  };
+
+  while (true) {
+    const vanishPlans = getVanishPlan(stack, rows, cols);
+    result.plan.push(vanishPlans);
+
+    if (vanishPlans.length === 0) {
+      break;
+    }
+
+    result.chain += 1;
+    result.score += calcChainStepScore(result.chain, vanishPlans);
+
+    const dropPlans = getDropPlan(stack, rows, cols);
+    result.plan.push(dropPlans);
+  }
+
+  return result;
+}
