768dcfc942eee03da864b24f1815c11a5b96d067
haripo <haripo@users.noreply.github.com>
2018-03-16 00:03:25 +0900

    Add new container for controls

---
+++ src/shared/components/SimulatorControls.js
@@ -0,0 +1,89 @@
+import React, { PureComponent } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { buttonColor, contentsMargin, controllerButtonSize } from '../../shared/utils/constants';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class SimulatorControls extends PureComponent {
+  render() {
+    return (
+      <View style={{ flexDirection: 'column' }}>
+        <View style={ styles.buttonGroup }>
+          <TouchableOpacity
+            style={ styles.controllerFullWidthButton }
+            onPress={ this.props.onUndoSelected }>
+            <Icon name="undo" size={30} color="#FFF" />
+            <Text style={{ color: '#FFF' }}>Undo</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={ styles.buttonGroup }>
+          <TouchableOpacity
+            style={ styles.controllerButton }
+            onPress={ this.props.onRotateLeftPressed }
+            disabled={ !this.props.isActive }>
+            <Icon name="rotate-left" size={30} color="#FFF" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={ styles.controllerButton }
+            onPress={ this.props.onRotateRightPressed }
+            disabled={ !this.props.isActive }>
+            <Icon name="rotate-right" size={30} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttons}>
+          <View style={ styles.buttonGroup }>
+            <TouchableOpacity
+              style={ styles.controllerButton }
+              onPress={ this.props.onMoveLeftPressed }
+              disabled={ !this.props.isActive }>
+              <Icon name="arrow-back" size={30} color="#FFF" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={ styles.controllerButton }
+              onPress={ this.props.onMoveRightPressed }
+              disabled={ !this.props.isActive }>
+              <Icon name="arrow-forward" size={30} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={ styles.controllerFullWidthButton }
+            onPress={ this.props.onDropPressed }
+            disabled={ !this.props.isActive }>
+            <Icon name="arrow-downward" size={30} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  controllerButton: {
+    width: controllerButtonSize,
+    height: controllerButtonSize,
+    marginTop: contentsMargin,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: buttonColor,
+    borderRadius: 3,
+    elevation: 3,
+  },
+  controllerFullWidthButton: {
+    backgroundColor: buttonColor,
+    width: controllerButtonSize * 2 + contentsMargin,
+    height: controllerButtonSize,
+    marginTop: contentsMargin,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    elevation: 3,
+  },
+  controllerButtonImage: {
+    width: '25%',
+    height: '25%',
+    resizeMode: 'contain'
+  },
+});
--- src/web/components/Simulator.js
+++ src/web/components/Simulator.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import NextWindowContainer from '../../shared/containers/NextWindowContainer';
 import ChainResultContainer from '../../shared/containers/ChainResultContainer';
-import { buttonColor, contentsMargin, contentsWidth, controllerButtonSize } from '../../shared/utils/constants';
+import { contentsMargin, contentsWidth } from '../../shared/utils/constants';
 import Field from '../../shared/components/Field';
 import HandlingPuyos from '../../shared/components/HandlingPuyos';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import SimulatorControls from '../../shared/components/SimulatorControls';
 
 export default class Simulator extends Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidMount() {
   }
 
   render() {
@@ -43,52 +40,15 @@ export default class Simulator extends Component {
               <NextWindowContainer />
               <ChainResultContainer />
             </View>
-            <View style={{ flexDirection: 'column' }}>
-              <View style={ styles.buttonGroup }>
-                <TouchableOpacity
-                  style={ styles.controllerFullWidthButton }
-                  onPress={ this.props.onUndoSelected }>
-                  <Icon name="undo" size={30} color="#FFF" />
-                  <Text style={{ color: '#FFF' }}>Undo</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={ styles.buttonGroup }>
-                <TouchableOpacity
-                  style={ styles.controllerButton }
-                  onPress={ this.props.onRotateLeftPressed }
-                  disabled={ !this.props.isActive }>
-                  <Icon name="rotate-left" size={30} color="#FFF" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={ styles.controllerButton }
-                  onPress={ this.props.onRotateRightPressed }
-                  disabled={ !this.props.isActive }>
-                  <Icon name="rotate-right" size={30} color="#FFF" />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.buttons}>
-                <View style={ styles.buttonGroup }>
-                  <TouchableOpacity
-                    style={ styles.controllerButton }
-                    onPress={ this.props.onMoveLeftPressed }
-                    disabled={ !this.props.isActive }>
-                    <Icon name="arrow-back" size={30} color="#FFF" />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={ styles.controllerButton }
-                    onPress={ this.props.onMoveRightPressed }
-                    disabled={ !this.props.isActive }>
-                    <Icon name="arrow-forward" size={30} color="#FFF" />
-                  </TouchableOpacity>
-                </View>
-                <TouchableOpacity
-                  style={ styles.controllerFullWidthButton }
-                  onPress={ this.props.onDropPressed }
-                  disabled={ !this.props.isActive }>
-                  <Icon name="arrow-downward" size={30} color="#FFF" />
-                </TouchableOpacity>
-              </View>
-            </View>
+            <SimulatorControls
+              onUndoSelected={ this.props.onUndoSelected }
+              onRotateLeftPressed={ this.props.onRotateLeftPressed }
+              onRotateRightPressed={ this.props.onRotateRightPressed }
+              onMoveLeftPressed={ this.props.onMoveLeftPressed }
+              onMoveRightPressed={ this.props.onMoveRightPressed }
+              onDropPressed={ this.props.onDropPressed }
+              isActive={ this.props.isActive }
+            />
           </View>
         </View>
       </View>
@@ -117,38 +77,4 @@ const styles = StyleSheet.create({
   sideHead: {
     flex: 1
   },
-  buttons: {
-  },
-  buttonGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  controllerButton: {
-    width: controllerButtonSize,
-    height: controllerButtonSize,
-    marginTop: contentsMargin,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: buttonColor,
-    borderRadius: 3,
-    elevation: 3,
-  },
-  controllerFullWidthButton: {
-    backgroundColor: buttonColor,
-    width: controllerButtonSize * 2 + contentsMargin,
-    height: controllerButtonSize,
-    marginTop: contentsMargin,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 3,
-    elevation: 3,
-  },
-  controllerButtonImage: {
-    width: '25%',
-    height: '25%',
-    resizeMode: 'contain'
-  },
-  field: {
-    margin: contentsMargin
-  }
 });
