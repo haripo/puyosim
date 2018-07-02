14229d8c451a09444608d2306587665977eb65f3
haripo <haripo@users.noreply.github.com>
2018-07-03 00:50:38 +0900

    Setup koa.js

--- index.api.js
+++ index.api.js
@@ -1,1 +1,16 @@
-console.log("YES!!!");
+import * as Koa from 'koa';
+
+import { routes } from './src/api/routes';
+
+const app = new Koa();
+
+const config = {
+  port: process.env.NODE_PORT || 3000,
+  prettyLog: process.env.NODE_ENV == 'development',
+};
+
+app.use(routes);
+
+app.listen(config.port);
+
+console.log(`Server running on port ${config.port}`);
