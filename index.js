fe7fde0b415251052caf41136f46eefe66146e11
haripo <haripo@users.noreply.github.com>
2018-07-17 01:22:51 +0900

    First iOS configuration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
 
@@ -18,19 +19,60 @@ Sentry.config('https://d03216589dad4ea9ac7a2431b901ff11:772e66c1594b42969f68d524
 const store = getStore(reducers, sagas);
 
 // Register screen components
+//Navigation.registerComponent('PuyoSimulator', () => Simulator, store, Provider);
 Navigation.registerComponent('com.puyosimulator.Simulator', () => Simulator, store, Provider);
 Navigation.registerComponent('com.puyosimulator.History', () => History, store, Provider);
 Navigation.registerComponent('com.puyosimulator.About', () => About, store, Provider);
 Navigation.registerComponent('com.puyosimulator.Settings', () => Settings, store, Provider);
 
 // launch first screen
-Navigation.startSingleScreenApp({
-  screen: {
-    screen: 'com.puyosimulator.Simulator',
-  },
-  appStyle: {
-    orientation: 'portrait'
-  },
-  animationType: 'none',
-  portraitOnlyMode: true,
-});
+// const App = () => {
+//   return Navigation.startSingleScreenApp({
+//     screen: {
+//       screen: 'com.puyosimulator.Simulator',
+//     },
+//     appStyle: {
+//       orientation: 'portrait'
+//     },
+//     animationType: 'none',
+//     portraitOnlyMode: true,
+//   });
+// };
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    Navigation.startSingleScreenApp({
+      screen: {
+        screen: 'com.puyosimulator.Simulator',
+      },
+      appStyle: {
+        orientation: 'portrait'
+      },
+      animationType: 'none',
+      portraitOnlyMode: true,
+    });
+  }
+
+  render() {
+    return (
+      <Text>
+        hogehgoe
+      </Text>
+    );
+  }
+}
+const app = new App();
+// Navigation.startSingleScreenApp({
+//   screen: {
+//     screen: 'com.puyosimulator.Simulator',
+//   },
+//   appStyle: {
+//     orientation: 'portrait'
+//   },
+//   animationType: 'none',
+//   portraitOnlyMode: true,
+// });
+
+// AppRegistry.registerComponent('PuyoSimulator', () => App);
+//
