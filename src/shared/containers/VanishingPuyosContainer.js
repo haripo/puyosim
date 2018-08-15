dedb0086efe7ffb1ffab621c721283ca1d7eb0ad
haripo <haripo@users.noreply.github.com>
2018-08-16 00:55:57 +0900

    Remove global constant puyoSize

--- src/shared/containers/VanishingPuyosContainer.js
+++ src/shared/containers/VanishingPuyosContainer.js
@@ -2,11 +2,13 @@ import { connect } from 'react-redux';
 import { applyGravity, finishVanishingAnimations } from '../actions/actions';
 import VanishingPuyos from '../components/VanishingPuyos';
 import { getVanishingPuyos } from '../selectors/simulatorSelectors';
+import { getLayout } from '../selectors/layoutSelectors';
 
 const mapStateToProps = (state) => {
   return {
     vanishings: getVanishingPuyos(state.simulator),
-    puyoSkin: state.config.puyoSkin
+    puyoSkin: state.config.puyoSkin,
+    layout: getLayout(state.layout)
   };
 };
 
