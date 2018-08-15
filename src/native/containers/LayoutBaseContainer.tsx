3e93401b25dcf22d94ec6b96ddf57a3129144797
haripo <haripo@users.noreply.github.com>
2018-08-16 00:15:05 +0900

    Add loading indicator while waiting layout

--- src/native/containers/LayoutBaseContainer.tsx
+++ src/native/containers/LayoutBaseContainer.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View } from "react-native";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import {
   resetLayout
 } from "../../shared/actions/actions";
+import { getTheme, Theme } from "../../shared/selectors/themeSelectors";
 
 type LayoutEvent = {
   nativeEvent: {
@@ -15,11 +16,17 @@ type LayoutEvent = {
 }
 
 type Props = {
+  theme: Theme,
   onLayout: (_: { width: number, height: number }) => void
+}
+
+type State = {
+  loading: boolean
 }
 
 const mapStateToProps = (state) => {
   return {
+    theme: getTheme(state.theme)
   };
 };
 
@@ -31,14 +38,30 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-class LayoutBaseContainer extends Component<Props, {}> {
+class LayoutBaseContainer extends Component<Props, State> {
   view: View | null = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
 
   handleLayout(e: LayoutEvent) {
     // e.nativeEvent にアクセスするとなぜかフリーズするので ref.measure を使う
     this.view!.measure((ox, oy, width, height) => {
       this.props.onLayout({ width, height });
+      this.setState({ loading: false });
     });
+  }
+
+  renderIndicator() {
+    return (
+      <View style={ styles.container }>
+        <ActivityIndicator size="small" color={ this.props.theme.themeColor } />
+      </View>
+    );
   }
 
   render() {
@@ -47,11 +70,18 @@ class LayoutBaseContainer extends Component<Props, {}> {
         style={{ flex: 1 }}
         onLayout={ this.handleLayout.bind(this) }
         ref={ r => this.view = r }>
-        { this.props.children }
+        { this.state.loading ? this.renderIndicator() : this.props.children }
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+});
 
 export default connect(
   mapStateToProps,
