f7f2019ea90d9aecb36657bccdf6e6c33a65da17
haripo <haripo@users.noreply.github.com>
2018-12-11 21:27:34 +0900

    Add IconButton component

--- src/shared/components/SimulatorControls.tsx
+++ src/shared/components/SimulatorControls.tsx
@@ -1,11 +1,8 @@
 import React, { PureComponent } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import {
-  buttonColor,
-  contentsMargin,
-} from '../utils/constants';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, View } from 'react-native';
+import { contentsMargin, } from '../utils/constants';
 import _ from 'lodash';
+import IconButton from "./IconButton";
 
 export type Props = {
   shortcuts?: { [key: string]: string },
@@ -22,84 +19,62 @@ export type Props = {
 };
 
 export default class SimulatorControls extends PureComponent<Props> {
-  renderShortcut(key) {
-    if (key) {
-      return (
-        <Text style={ styles.shortcut }>{ key }</Text>
-      )
-    }
-    return null;
-  }
-
   render() {
     const shortcuts = _.mapValues(this.props.shortcuts || {}, (shortcut) => `(${shortcut})`);
 
     return (
       <View style={ styles.component }>
         <View style={ styles.buttonGroup }>
-          <TouchableOpacity
+          <IconButton
             disabled={ !this.props.canUndo }
-            style={ styles.controllerButton }
-            activeOpacity={ this.props.canUndo ? 0.7 : 1 }
-            // onClick={ this.props.onUndoSelected }
-            onPress={ this.props.onUndoSelected }>
-            <Icon name="undo" size={ 30 } color="#FFF"/>
-            <Text style={ { color: '#FFF' } }>Undo</Text>
-            { this.renderShortcut(shortcuts.undo) }
-          </TouchableOpacity>
-          <TouchableOpacity
+            icon='undo'
+            text='undo'
+            onPressed={ this.props.onUndoSelected }
+            shortcutText={ shortcuts.undo }
+          />
+          <IconButton
+            style={ styles.controllerRightButton }
             disabled={ !this.props.canRedo }
-            style={ [styles.controllerButton, styles.controllerRightButton] }
-            activeOpacity={ this.props.canRedo ? 0.7 : 1 }
-            // onClick={ this.props.onRedoSelected }
-            onPress={ this.props.onRedoSelected }>
-            <Icon name="redo" size={ 30 } color="#FFF"/>
-            <Text style={ { color: '#FFF' } }>Redo</Text>
-            { this.renderShortcut(shortcuts.redo) }
-          </TouchableOpacity>
+            icon='redo'
+            text='redo'
+            onPressed={ this.props.onRedoSelected }
+            shortcutText={ shortcuts.redo }
+          />
         </View>
         <View style={ styles.buttonGroup }>
-          <TouchableOpacity
-            style={ styles.controllerButton }
-            // onClick={ this.props.onRotateLeftPressed }
-            onPress={ this.props.onRotateLeftPressed }
-            disabled={ !this.props.isActive }>
-            <Icon name="rotate-left" size={ 30 } color="#FFF"/>
-            { this.renderShortcut(shortcuts.rotateLeft) }
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={ [styles.controllerButton, styles.controllerRightButton] }
-            // onClick={ this.props.onRotateRightPressed }
-            onPress={ this.props.onRotateRightPressed }
-            disabled={ !this.props.isActive }>
-            <Icon name="rotate-right" size={ 30 } color="#FFF"/>
-            { this.renderShortcut(shortcuts.rotateRight) }
-          </TouchableOpacity>
+          <IconButton
+            disabled={ !this.props.isActive }
+            icon='rotate-left'
+            onPressed={ this.props.onRotateLeftPressed }
+            shortcutText={ shortcuts.rotateLeft }
+          />
+          <IconButton
+            style={ styles.controllerRightButton }
+            disabled={ !this.props.isActive }
+            icon='rotate-right'
+            onPressed={ this.props.onRotateRightPressed }
+            shortcutText={ shortcuts.rotateRight }
+          />
         </View>
         <View style={ styles.buttonGroup }>
-          <TouchableOpacity
-            style={ styles.controllerButton }
-            // onClick={ this.props.onMoveLeftPressed }
-            onPress={ this.props.onMoveLeftPressed }
-            disabled={ !this.props.isActive }>
-            <Icon name="arrow-back" size={ 30 } color="#FFF"/>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={ [styles.controllerButton, styles.controllerRightButton] }
-            // onClick={ this.props.onMoveRightPressed }
-            onPress={ this.props.onMoveRightPressed }
-            disabled={ !this.props.isActive }>
-            <Icon name="arrow-forward" size={ 30 } color="#FFF"/>
-          </TouchableOpacity>
+          <IconButton
+            disabled={ !this.props.isActive }
+            icon='arrow-back'
+            onPressed={ this.props.onMoveLeftPressed }
+          />
+          <IconButton
+            style={ styles.controllerRightButton }
+            disabled={ !this.props.isActive }
+            icon='arrow-forward'
+            onPressed={ this.props.onMoveRightPressed }
+          />
         </View>
         <View style={ styles.buttonGroup }>
-          <TouchableOpacity
-            style={ styles.controllerFullWidthButton }
-            // onClick={ this.props.onDropPressed }
-            onPress={ this.props.onDropPressed }
-            disabled={ !this.props.isActive }>
-            <Icon name="arrow-downward" size={ 30 } color="#FFF"/>
-          </TouchableOpacity>
+          <IconButton
+            disabled={ !this.props.isActive }
+            icon='arrow-downward'
+            onPressed={ this.props.onDropPressed }
+          />
         </View>
       </View>
     );
@@ -116,34 +91,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  controllerButton: {
-    flex: 1,
-    marginTop: contentsMargin,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: buttonColor,
-    borderRadius: 3,
-    elevation: 3,
-  },
   controllerRightButton: {
     marginLeft: contentsMargin
-  },
-  controllerFullWidthButton: {
-    flex: 1,
-    backgroundColor: buttonColor,
-    marginTop: contentsMargin,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 3,
-    elevation: 3,
-  },
-  controllerButtonImage: {
-    width: '25%',
-    height: '25%',
-    resizeMode: 'contain'
-  },
-  shortcut: {
-    color: 'white',
-    fontSize: 12
   }
 });
