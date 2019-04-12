a7d2f1418291462637a428355f2767aed511e909
haripo <haripo@users.noreply.github.com>
2019-04-13 01:36:35 +0900

    Add @babel/plugin-proposal-class-property dependency to run tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-{
+module.exports =  {
   "presets": [
       "module:metro-react-native-babel-preset",
       "module:react-native-dotenv"
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-class-properties"
   ]
-}
+};
