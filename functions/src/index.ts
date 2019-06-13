6e676a68fda032b462c3f6123f60b744b0cd15ff
haripo <haripo@users.noreply.github.com>
2019-06-14 00:31:37 +0900

    Change endpoint name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,24 +8,37 @@ const runtimeOptions: functions.RuntimeOptions = {
   memory: '1GB'
 };
 
-export const helloWorld = functions.runWith(runtimeOptions).https.onRequest(async (request, response) => {
-  const theme = {
-    cardBackgroundColor: '#EFEBE9',
-    buttonColor: '#8D6E63',
-    themeColor: '#6D4C41',
-    themeLightColor: '#EFEBE9'
-  };
+const theme = {
+  cardBackgroundColor: '#EFEBE9',
+  buttonColor: '#8D6E63',
+  themeColor: '#6D4C41',
+  themeLightColor: '#EFEBE9'
+};
 
+function parseHistory(strQueue: string, strHistory: string) {
+  const queue = deserializeQueue(strQueue);
+  const minimumHistory = deserializeHistoryRecords(strHistory);
+  return createHistoryFromMinimumHistory(minimumHistory, queue, minimumHistory.length - 1);
+}
+
+export const renderGifMovie = functions.runWith(runtimeOptions).https.onRequest(async (request, response) => {
+  const strQueue = request.query.q;
+  const strHistory = request.query.h;
+  const skin = request.query.skin || 'puyoSkinDefault';
+
+  const history = parseHistory(strQueue, strHistory);
+  const renderer = new FieldImageRenderer(theme, skin);
+  const buf = await renderer.renderVideo(history);
+
+  response.contentType('image/gif');
+  response.send(buf);
+});
+
+export const renderGifMovieDebug = functions.runWith(runtimeOptions).https.onRequest(async (request, response) => {
   const strQueue = 'DEpGGxwjkswFyppxxzsGiqqizFDrDxkkxpFlyGDEjDzjjrjysjwxzqljDwxGjFkzrrpyqjDDislyDsFFikDkqyGxEGljrzxpFqjDFDxqziwkjsFzixGjksFlxsDkyEpr';
   const strHistory = 'mmiqsognsjakfklqfqedfdartlnornmagmmobdoep9';
 
-  // const strQueue = request.query.q;
-  // const strHistory = request.query.h;
-
-  const queue = deserializeQueue(strQueue);
-  const minimumHistory = deserializeHistoryRecords(strHistory);
-  const history = createHistoryFromMinimumHistory(minimumHistory, queue, minimumHistory.length - 1);
-
+  const history = parseHistory(strQueue, strHistory);
   const renderer = new FieldImageRenderer(theme, 'puyoSkinDefault');
   const buf = await renderer.renderVideo(history);
 
