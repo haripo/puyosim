f35314a2a1b65d454a0d450c68cb11a113ca3887
haripo <haripo@users.noreply.github.com>
2019-06-24 16:18:26 +0900

    Fix share view for iOS

--- src/native/components/ShareOption.tsx
+++ src/native/components/ShareOption.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Share from 'react-native-share';
 
 import { Button, Left, ListItem, Radio, Right, Text as NativeBaseText } from 'native-base';
@@ -58,67 +58,65 @@ export default class ShareOption extends Component<Props, State> {
   render() {
     const option = this.props.shareOption;
     return (
-      <View style={ styles.container }>
+      <SafeAreaView style={ styles.container }>
         <View style={ styles.contents }>
-          <ScrollView>
-            <Text style={ styles.radioTitle }>{ t('shareUrlType') }</Text>
-            <ListItem onPress={ this.handleUrlOptionChanged.bind(this, 'none') }>
-              <Left>
-                <Text>{ t('shareUrlTypeNone') }</Text>
-              </Left>
-              <Right>
-                <Radio selected={ option.hasUrl === 'none' } />
-              </Right>
-            </ListItem>
-            <ListItem onPress={ this.handleUrlOptionChanged.bind(this, 'current') }>
-              <Left>
-                <Text>{ t('shareUrlTypeSimple') }</Text>
-              </Left>
-              <Right>
-                <Radio selected={ option.hasUrl === 'current' } />
-              </Right>
-            </ListItem>
+            <ScrollView>
+              <Text style={ styles.radioTitle }>{ t('shareUrlType') }</Text>
+              <ListItem onPress={ this.handleUrlOptionChanged.bind(this, 'none') } style={ styles.radioItem }>
+                <Left>
+                  <Text>{ t('shareUrlTypeNone') }</Text>
+                </Left>
+                <Right>
+                  <Radio selected={ option.hasUrl === 'none' } />
+                </Right>
+              </ListItem>
+              <ListItem onPress={ this.handleUrlOptionChanged.bind(this, 'current') } style={ styles.radioItem }>
+                <Left>
+                  <Text>{ t('shareUrlTypeSimple') }</Text>
+                </Left>
+                <Right>
+                  <Radio selected={ option.hasUrl === 'current' } />
+                </Right>
+              </ListItem>
 
-            <Text style={ styles.radioTitle }>{ t('shareMediaType') }</Text>
-            <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'none') }>
-              <Left>
-                <Text>{ t('shareMediaTypeNone') }</Text>
-              </Left>
-              <Right>
-                <Radio selected={ option.hasMedia === 'none' } />
-              </Right>
-            </ListItem>
-            <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'image') }>
-              <Left>
-                <Text>{ t('shareMediaTypeImage') }</Text>
-              </Left>
-              <Right>
-                <Radio selected={ option.hasMedia === 'image' } />
-              </Right>
-            </ListItem>
-            <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'video') }>
-              <Left>
-                <Text>{ t('shareMediaTypeMovie') }</Text>
-              </Left>
-              <Right>
-                <Radio selected={ option.hasMedia === 'video' } />
-              </Right>
-            </ListItem>
-          </ScrollView>
-
-          <Button
-            primary
-            full
-            style={ styles.shareButton }
-            onPress={ this.handleSharePressed.bind(this) }
-            disabled={ this.props.shareOption.hasMedia === 'none' && this.props.shareOption.hasUrl === 'none' }
-          >
-            <NativeBaseText>{ t('confirmShare') }</NativeBaseText>
-          </Button>
+              <Text style={ styles.radioTitle }>{ t('shareMediaType') }</Text>
+              <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'none') } style={ styles.radioItem }>
+                <Left>
+                  <Text>{ t('shareMediaTypeNone') }</Text>
+                </Left>
+                <Right>
+                  <Radio selected={ option.hasMedia === 'none' } />
+                </Right>
+              </ListItem>
+              <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'image') } style={ styles.radioItem }>
+                <Left>
+                  <Text>{ t('shareMediaTypeImage') }</Text>
+                </Left>
+                <Right>
+                  <Radio selected={ option.hasMedia === 'image' } />
+                </Right>
+              </ListItem>
+              <ListItem onPress={ this.handleMediaOptionChanged.bind(this, 'video') } style={ styles.radioItem }>
+                <Left>
+                  <Text>{ t('shareMediaTypeMovie') }</Text>
+                </Left>
+                <Right>
+                  <Radio selected={ option.hasMedia === 'video' } />
+                </Right>
+              </ListItem>
+            </ScrollView>
+            <Button
+              primary
+              full
+              style={ styles.shareButton }
+              onPress={ this.handleSharePressed.bind(this) }
+              disabled={ this.props.shareOption.hasMedia === 'none' && this.props.shareOption.hasUrl === 'none' }
+            >
+              <NativeBaseText>{ t('confirmShare') }</NativeBaseText>
+            </Button>
         </View>
-
         <ModalIndicator visible={ this.props.isGenerating } text={ t('shareProcessing') } />
-      </View>
+      </SafeAreaView>
     );
   }
 }
@@ -139,6 +137,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 10,
     marginTop: 20
+  },
+  radioItem: {
+    height: 54
   },
   shareButton: {
     margin: 8,
