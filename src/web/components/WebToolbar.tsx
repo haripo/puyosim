62b0fbbfea8050a4f7e2cfa7122ba9213d4c63ef
haripo <haripo@users.noreply.github.com>
2019-07-15 00:17:26 +0900

    Add app link

--- src/web/components/WebToolbar.tsx
+++ src/web/components/WebToolbar.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { themeColor, themeLightColor, webToolbarSize } from '../../shared/utils/constants';
+
+function openLink(url: string) {
+  // @ts-ignore
+  window.open(url, "_blank");
+}
 
 type Props = {
 }
@@ -14,7 +19,24 @@ export default class WebToolbar extends React.Component<Props, {}> {
             puyosim
           </Text>
           <View style={ styles.navigation }>
-          {/* 右側に表示されるナビゲーション */}
+            <View style={ styles.navigationItem }>
+              <Text style={ { color: themeLightColor } }>
+                Also available on
+              </Text>
+              <TouchableOpacity onPress={ () => openLink('https://apps.apple.com/us/app/puyosim/id1435074935') }>
+                <Text style={ { color: themeLightColor, margin: 3 } }>
+                  iOS
+                </Text>
+              </TouchableOpacity>
+              <Text style={ { color: themeLightColor } }>
+                /
+              </Text>
+              <TouchableOpacity onPress={ () => openLink('https://play.google.com/store/apps/details?id=com.puyosimulator') }>
+                <Text style={ { color: themeLightColor, margin: 3 } }>
+                  Android
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -45,7 +67,8 @@ const styles = StyleSheet.create({
     marginRight: 16
   },
   navigationItem: {
-    lineHeight: webToolbarSize - 3,
-    color: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 });
